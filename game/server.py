05a41e4471b58bc868ea0bae5e7292e49e2edb3d
onehana1 <73771420+onehana1@users.noreply.github.com>
2021-11-24 23:37:54 +0900

    s

--- game/server.py
+++ game/server.py
@@ -1,2 +1,38 @@
 # 서버 만들고
 # 충돌처리 py만들기
+
+
+boy = None
+fire = None
+
+
+
+
+grass = None
+sky = None
+sky = None
+
+
+stage1_ground1 = None
+stage1_ground2 = None
+
+
+box = None
+box2 = None
+
+block = None
+
+mushroom = None
+flower = None
+coin = None
+
+
+
+
+
+gumba = None
+koopas = None
+redkoopas = None
+
+
+gumbas = []
