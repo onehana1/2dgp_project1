2223cc6bee4b1cd4908aff68b3d2a39255229f90
onehana1 <73771420+onehana1@users.noreply.github.com>
2021-11-28 01:05:23 +0900

    Update collition.py

--- game/collition.py
+++ game/collition.py
@@ -1,1 +1,74 @@
 #충돌처리 옮기기
+
+
+def collide(a,b):
+    left_a, bottom_a, right_a, top_a = a.crush_box()
+    left_b, bottom_b, right_b, top_b = b.crush_box()
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    return True
+
+
+
+
+def collide_floor(a,b):
+    left_a, bottom_a, right_a, top_a = a.crush_box()
+    left_b, bottom_b, right_b, top_b = b.crush_box()
+
+    if left_a > right_b: return False
+    elif right_a < left_b: return False
+    elif top_a < bottom_b: return False
+    elif bottom_a > top_b: return False
+
+    elif bottom_a <= top_b: return True #발로 밟기
+
+
+
+def collide_head(a,b):
+    left_a, bottom_a, right_a, top_a = a.crush_box()
+    left_b, bottom_b, right_b, top_b = b.crush_box()
+
+    
+    if left_a > right_b: return False
+    elif right_a < left_b: return False
+    elif top_a < bottom_b: return False
+    elif bottom_a > top_b: return False
+
+    elif top_a >= bottom_b: return True #머리 박기
+
+    
+
+def collide_monster(a,b):
+    left_a, bottom_a, right_a, top_a = a.crush_box()
+    left_b, bottom_b, right_b, top_b = b.crush_box()
+
+    
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    #if bottom_a > top_b: return False
+
+
+    return True
+
+def collide_head_monster(a,b):
+    left_a, bottom_a, right_a, top_a = a.crush_box()
+    left_b, bottom_b, right_b, top_b = b.crush_box()
+
+    
+    if left_a > right_b: return False
+    if right_a < left_b: return False
+    if top_a < bottom_b: return False
+    if bottom_a > top_b: return False
+
+    if bottom_a <= top_b: 
+        if top_a > top_b:
+            return False
+
+        return True
+
+
+
